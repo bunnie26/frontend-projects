9d49ff0b94d3420291feae0fb4447f0edc205771
bunnie26 <66902288+bunnie26@users.noreply.github.com>
2021-08-17 02:23:52 +0530

    random

--- landing page/index.js
+++ landing page/index.js
@@ -1,9 +1,19 @@
 var header = document.querySelector(".points");
 var btns = header.querySelectorAll(".btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
 }
+function addCart() {
+  nav_div.classList.toggle("not-visible");
+  console.log("hhh");
+  var ham = document.querySelector(".hamburger");
+  ham.classList.toggle("activeHam");
+  console.log(ham);
+}
+var cart = document.getElementById("gg");
+var nav_div = document.getElementById("cart");
+// nav_div.addEventListener("click", addCart);
